5389bb3413b1d4c55c6696d1de52b20f40602a88
ArthurCoelho0 <49699226+ArthurCoelho0@users.noreply.github.com>
2019-05-09 19:30:44 -0300

    id and else

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,16 @@
+function alfabetoMilitar(n) {
+    if(n==="a" || n==="A")
+    return("alpha")
+    if(n==="e" || n==="E")
+    return("echo")
+    if(n==="i" || n==="I")
+    return("india")
+    if(n==="o" || n==="O")
+    return("oscar")
+    if(n==="u" || n==="U")
+    return("uniform")
+    else
+    return("false")
+ }
+
+
