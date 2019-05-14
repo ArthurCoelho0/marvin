37ea3921340c43bb949f28657509b184b1687080
ArthurCoelho0 <49699226+ArthurCoelho0@users.noreply.github.com>
2019-05-14 19:38:47 -0300

    var e if

--- fase04/ex01/nove.js
+++ fase04/ex01/nove.js
@@ -1,9 +1,8 @@
-function nove(n) {
-    if (n < 0)
-    n = n * -1
-    if (n % 10 === 9)
+function nove(n)  {
+    var y = n.toString()
+    var x =y.charAt(y.length-1)
+    if (x == 9)
     return (true)
     else
     return (false)
-  
 }
