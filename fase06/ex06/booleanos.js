3173524a5d8c1110a28458322f6a5df444facd40
ArthurCoelho0 <49699226+ArthurCoelho0@users.noreply.github.com>
2019-05-29 20:50:46 -0300

    arrays

---
+++ fase06/ex06/booleanos.js
@@ -0,0 +1,15 @@
+function booleanos(array) {
+    tey = []
+    i = 0
+    u = 0
+    m = array.length
+    while(m > i){
+        if (typeof array[i] !== "boolean"){
+           tey[u] = array [i]
+           u++
+        }
+    
+    i++
+    }
+    return tey
+}
